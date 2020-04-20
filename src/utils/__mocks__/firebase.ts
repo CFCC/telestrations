824cd77f6f4011e872b6d81c24081b7ca07bdc83
Jordan Pitlor <jordan@jordanpitlor.com>
2020-04-20 14:47:23 -0400

    fix firebase mock

--- src/utils/__mocks__/firebase.ts
+++ src/utils/__mocks__/firebase.ts
@@ -1,31 +1,25 @@
+const snapshot = {
+    onSnapshot: () => {},
+}
+const ref = {
+    ...snapshot,
+    where: () => snapshot,
+}
+
 export const firebaseLoginUi = {
     start: jest.fn(),
 };
 
-export const playerRef = (gameCode: string, playerId: string) => {
+export const playerRef = (gameCode: string, playerId: string) => ref;
 
-};
+export const playerListRef = (gameCode: string) => ref;
 
-export const playerListRef = (gameCode: string) => {
+export const notebookRef = (gameCode: string, notepadId: string) => ref;
 
-};
+export const notebookListRef = (gameCode: string) => ref;
 
-export const notebookRef = (gameCode: string, notepadId: string) => {
+export const gameListRef = () => ref;
 
-};
+export const gameRef = (gameCode: string) => ref;
 
-export const notebookListRef = (gameCode: string) => {
-
-};
-
-export const gameListRef = () => {
-
-};
-
-export const gameRef = (gameCode: string) => {
-
-};
-
-export const storageRef = (fileName: string) => {
-
-};
+export const storageRef = (fileName: string) => ref;
